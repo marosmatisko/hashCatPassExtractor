50e55ad1291babd98f794d095b01f1f3b68363ca
Administrator <admin@Administrators-Mac.local>
2017-04-27 11:57:08 +0200

    suppresed output

--- extractor.sh
+++ extractor.sh
@@ -1,22 +1,21 @@
 #!bin/bash
 
 username="$1";
-rm -rf $username.plist;
-rm -rf shadowhash;
-rm -rf entropy;
 cp /var/db/dslocal/nodes/Default/users/$username.plist $username.plist;
 chmod 777 $username.plist;
 plutil -convert xml1 $username.plist;
 python shadowHashData.py $username;
-file shadowhash;
+#file shadowhash;
 plutil -convert xml1 shadowhash;
-file shadowhash;
+#file shadowhash;
 python entropy.py;
-file entropy;
+#file entropy;
 xxd -ps entropy entropy2;
-file salt;
+#file salt;
 xxd -ps salt salt2;
 python final.py;
-cat hash.txt;
-echo;
+#cat hash.txt;
+#echo;
+rm -rf $username.plist shadowhash entropy;
+rm -rf entropy2 salt salt2;
 exit 0;
